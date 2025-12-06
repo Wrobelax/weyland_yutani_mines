235cb4a156ad1d2b893b8ca25d4e41e0e4703480
Adrian <awrobel.wr@gmail.com>
2025-12-06 19:55:06 +0100

    Fixed error with importing module.

--- data/loader.py
+++ data/loader.py
@@ -6,6 +6,7 @@ import streamlit as st
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
+from collections.abc import Mapping
 
 #-----------------------------
 # Load data from Google Sheets
